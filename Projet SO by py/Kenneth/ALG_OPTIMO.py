3ac4ced57e6dd8cc07ab5ea7597f0a07a08f8af7
DEEON-04zz <carpetchavarria04@gmail.com>
2026-05-26 11:36:31 -0600

    Optimo.py

---
+++ Projet SO by py/Kenneth/ALG_OPTIMO.py
@@ -0,0 +1,8 @@
+
+def ejecutar():
+    print("\n=== Remplazo Óptimo ===\n")
+    
+    Marcos = int(input("Ingrese el número de marcos de página: "))
+    entrada = input("Ingrese las páginas a cargar: ")
+    referencia = list(map(int, entrada.split()))
+    
